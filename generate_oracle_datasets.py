824a9c5fda763fee27b7117586c82d4022f456c7
Charles Lu <charleslu@live.com>
2017-06-03 07:59:54 +0000

    stub oracle dataset generation

--- generate_oracle_datasets.py
+++ generate_oracle_datasets.py
@@ -1,6 +1,16 @@
 import pickle
 import json
 
+# load pytorch etc here
+
 import data
 
-with open(data.get_gw_file('train'), 'r') as f:
+# add functions to get processed features here
+
+if __name__ == '__main__':
+    # load pretrained model here
+    
+    for split in ('train', 'valid', 'test'):
+        with open(data.get_gw_file(split), 'r') as f:
+            for line in f:
+                example = json.loads(line)
