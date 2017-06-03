8091563645d002e7f1778a6d9b407fe9657fee90
Charles Lu <charleslu@live.com>
2017-06-03 06:13:12 +0000

    Add coco images

--- datasets/get_datasets.sh
+++ datasets/get_datasets.sh
@@ -1,2 +1,20 @@
 wget https://s3-us-west-2.amazonaws.com/guess-what/guesswhat.train.jsonl.gz https://s3-us-west-2.amazonaws.com/guess-what/guesswhat.valid.jsonl.gz https://s3-us-west-2.amazonaws.com/guess-what/guesswhat.test.jsonl.gz
 gunzip *.gz
+
+mkdir coco
+cd coco
+wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip http://msvocds.blob.core.windows.net/coco2014/val2014.zip
+
+unzip train2014.zip
+cd train2014
+echo *.jpg | xargs mv -t ..
+cd ..
+rm -rf train2014/
+rm train2014.zip
+
+unzip val2014.zip
+cd val2014
+echo *.jpg | xargs mv -t ..
+cd ..
+rm -rf val2014/
+rm val2014.zip
