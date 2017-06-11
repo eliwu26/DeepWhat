3c66686b7317df97305530afd81a682e65e1e699
Charles Lu <charleslu@live.com>
2017-06-11 06:08:45 +0000

    Cleanup oracle

--- models/oracle.py
+++ models/oracle.py
@@ -9,15 +9,14 @@ import data
 vocab_map = vocab.VocabMap()
 vocab_size = vocab_map.vocab_size
 
+RESNET_FEATURE_SIZE = 2048
+SPATIAL_SIZE = 8
+
 class OracleNet(nn.Module):
-    def __init__(self, image_spatial_dim=4104,
-                 question_hidden_dim=128, token_embed_dim=64,
-                 vocab_size=vocab_size, question_max_len=data.MAX_TOKENS_PER_QUESTION,
-                 num_categories=data.NUM_CATEGORIES, category_embed_dim=32):
+    def __init__(self, question_hidden_dim=128, token_embed_dim=64, category_embed_dim=32,
+                 vocab_size=vocab_size, num_categories=data.NUM_CATEGORIES):
         super(OracleNet, self).__init__()
         
-        self.image_spatial_dim = image_spatial_dim
-        self.question_max_len = question_max_len
         self.question_hidden_dim = question_hidden_dim
         
         self.token_embedding = nn.Embedding(
@@ -37,8 +36,9 @@ class OracleNet(nn.Module):
             batch_first=True
         )
         
-        fc1_in = image_spatial_dim + question_hidden_dim + category_embed_dim
-        self.fc1 = nn.Linear(fc1_in, 128)
+        fc_in_dim = 2 * RESNET_FEATURE_SIZE + SPATIAL_SIZE + question_hidden_dim + category_embed_dim
+        
+        self.fc1 = nn.Linear(fc_in_dim, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 3)
         #self.dout = nn.Dropout()
@@ -73,34 +73,3 @@ class OracleNet(nn.Module):
         self.optimizer.step()
         
         return loss
-        
-# USING PACKED SEQUENCE
-#     def sort_by_len(self, embed_tokens, question_lens, features, categories):
-#         question_lens, indices = question_lens.sort(descending=True)
-#         print(question_lens.data.numpy())
-#         print(question_lens.size())
-#         embed_tokens = embed_tokens.index_select(dim=0, index=indices)
-#         print(embed_tokens.size())
-#         features = features.index_select(dim=0, index=indices)
-#         categories = categories.index_select(dim=0, index=indices)
-#         return (
-#             nn.utils.rnn.pack_padded_sequence(
-#                 embed_tokens,
-#                 question_lens,
-#                 batch_first=True
-#             ),
-#             features,
-#             categories
-#         )
-        
-#     def forward(self, tokens, question_lens, features, categories):
-#         embed_category = self.category_embedding(categories)
-#         embed_tokens = self.token_embedding(tokens)
-        
-#         questions, features, categories = self.sort_by_len(embed_tokens, question_lens, features, categories)
-        
-#         output, h_n = self.question_encoder(embed_tokens)
-#         print(output.size())
-#         print((question_lens-1).size())
-#         question_encodings = output[:, question_lens-1, :]
-#         print(question_encodings.size())
